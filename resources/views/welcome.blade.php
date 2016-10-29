7e56a3df0187aa90b5bebbfdea035a2e42ad6ffd
Ron Shalevich <ronshalevich@Rons-MacBook-Air.local>
2016-10-28 17:06:15 -0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>calculator</title>
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootswatch/3.3.2/cyborg/bootstrap.min.css"/>
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/css/app.css"/>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    <script src="/js/app.js"></script>
+  </head>
+  <body>
+    <div id="calculator">
+	    <div class="top">
+		    <span class="clear">C</span>
+		    <div class="screen"></div>
+	    </div>
+	    <div class="keys">
+    		<span>7</span>
+    		<span>8</span>
+    		<span>9</span>
+    		<span class="operator">+</span>
+    		<span>4</span>
+    		<span>5</span>
+    		<span>6</span>
+    		<span class="operator">-</span>
+    		<span>1</span>
+    		<span>2</span>
+    		<span>3</span>
+    		<span class="operator">÷</span>
+    		<span>0</span>
+    		<span>.</span>
+    		<span class="eval">=</span>
+    		<span class="operator">x</span>
+	    </div>
+    </div>
+  </body>
+</html>
